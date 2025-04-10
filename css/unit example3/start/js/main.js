d6fc0248027c497fcce3fbe732b787e4470357af
homin <ghals1447@naver.com>
2025-04-10 17:45:29 +0900

    Delete css directory

--- css/unit example3/start/js/main.js
+++
@@ -1,8 +0,0 @@
-﻿$(function(){
-	setInterval(function(){
-		$(".slider ul").animate({ left: '-100vw' }, 500, function(){
-			$(".slider ul").append($(".slider ul li:first-child"));
-			$(".slider ul").css({ left: 0 });
-		});
-	}, 3000);
-});
